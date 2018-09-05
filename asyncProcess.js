d165643eb8c84a96d7145fb3d3ca6830ac36e5f5
Chris Stead <cmstead@gmail.com>
2018-09-05 11:34:21 -0700

    Extract double numbers

--- asyncProcess.js
+++ asyncProcess.js
@@ -4,37 +4,42 @@ function asyncProcess(asyncApi, logger) {
 
     function doAsyncStuff(callback) {
         let lastState = null;
-        asyncApi.getNumbers(function (error, ...args) {
-            if (error) {
-                logger.log('An error occurred!', error);
-                callback(error, lastState);
-            } else {
-                asyncApi.doubleNumbers(function (error, ...args) {
-                    if (error) {
-                        logger.log('An error occurred!', error);
-                        callback(error, lastState);
-                    } else {
-                        asyncApi.alwaysFails(function (error, ...args) {
-                            if (error) {
-                                logger.log('An error occurred!', error);
-                                callback(error, lastState);
-                            } else {
-                                asyncApi.logMessage(function (error, ...args) {
-                                    if (error) {
-                                        logger.log('An error occurred!', error);
-                                        callback(error, lastState);
-                                    } else {
-                                        callback(null, lastState);
-                                    }
-                                });
-                            }
-                        });
-                    }
-                });
-            }
-        });
+        
+        function doubleNumbers(error, args) {
+            return function (error, ...args) {
+                if (error) {
+                    logger.log('An error occurred!', error);
+                    callback(error, lastState);
+                } else {
+                    asyncApi.doubleNumbers(function (error, ...args) {
+                        if (error) {
+                            logger.log('An error occurred!', error);
+                            callback(error, lastState);
+                        } else {
+                            asyncApi.alwaysFails(function (error, ...args) {
+                                if (error) {
+                                    logger.log('An error occurred!', error);
+                                    callback(error, lastState);
+                                } else {
+                                    asyncApi.logMessage(function (error, ...args) {
+                                        if (error) {
+                                            logger.log('An error occurred!', error);
+                                            callback(error, lastState);
+                                        } else {
+                                            callback(null, lastState);
+                                        }
+                                    });
+                                }
+                            });
+                        }
+                    });
+                }
+            };
+        }
+
+        asyncApi.getNumbers(doubleNumbers(error, args));
     }
-    
+
     return {
         doAsyncStuff: doAsyncStuff
     };
