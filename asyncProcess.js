6a68df26d1d3f90666a7773f77ad3bd60b159271
Chris Stead <cmstead@gmail.com>
2018-09-05 11:42:58 -0700

    Lift always fails function

--- asyncProcess.js
+++ asyncProcess.js
@@ -4,34 +4,36 @@ function asyncProcess(asyncApi, logger) {
 
     function doAsyncStuff(callback) {
         let lastState = null;
-        
+
+        function alwaysFails(error, ...args) {
+            if (error) {
+                logger.log('An error occurred!', error);
+                callback(error, lastState);
+            } else {
+                asyncApi.alwaysFails(function (error, ...args) {
+                    if (error) {
+                        logger.log('An error occurred!', error);
+                        callback(error, lastState);
+                    } else {
+                        asyncApi.logMessage(function (error, ...args) {
+                            if (error) {
+                                logger.log('An error occurred!', error);
+                                callback(error, lastState);
+                            } else {
+                                callback(null, lastState);
+                            }
+                        });
+                    }
+                });
+            }
+        }
+
         function doubleNumbers(error, ...args) {
             if (error) {
                 logger.log('An error occurred!', error);
                 callback(error, lastState);
             } else {
-                asyncApi.doubleNumbers(function (error, ...args) {
-                    if (error) {
-                        logger.log('An error occurred!', error);
-                        callback(error, lastState);
-                    } else {
-                        asyncApi.alwaysFails(function (error, ...args) {
-                            if (error) {
-                                logger.log('An error occurred!', error);
-                                callback(error, lastState);
-                            } else {
-                                asyncApi.logMessage(function (error, ...args) {
-                                    if (error) {
-                                        logger.log('An error occurred!', error);
-                                        callback(error, lastState);
-                                    } else {
-                                        callback(null, lastState);
-                                    }
-                                });
-                            }
-                        });
-                    }
-                });
+                asyncApi.doubleNumbers(alwaysFails);
             }
         }
 
