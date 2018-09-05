9bbff3e7f65266bb157f72d5d8c141f03e7a096c
Chris Stead <cmstead@gmail.com>
2018-09-05 12:27:36 -0700

    Back to initial state

--- asyncProcess.js
+++ asyncProcess.js
@@ -2,43 +2,39 @@
 
 function asyncProcess(asyncApi, logger) {
 
-    function errorOrNext(nextFunction, callback) {
-        return function (error, ...args) {
-            if (error) {
-                callback(error);
-            } else {
-                nextFunction.apply(null, args.concat([callback]));
-            }
-        }
-    }
-
-    function waterfall(asyncFunctions, finallyFunction) {
-        return asyncFunctions
-            .slice(0)
-            .reverse(0)
-            .reduce(function (compositeFunction, nextFunction) {
-                return errorOrNext(nextFunction, compositeFunction);
-            }, finallyFunction);
-    }
-
     function doAsyncStuff(callback) {
         let lastState = null;
-
-        function finishAsyncProcess(error, ...args) {
+        asyncApi.getNumbers(function (error, ...args) {
             if (error) {
                 logger.log('An error occurred!', error);
+                callback(error, lastState);
+            } else {
+                asyncApi.doubleNumbers(function (error, ...args) {
+                    if (error) {
+                        logger.log('An error occurred!', error);
+                        callback(error, lastState);
+                    } else {
+                        asyncApi.alwaysFails(function (error, ...args) {
+                            if (error) {
+                                logger.log('An error occurred!', error);
+                                callback(error, lastState);
+                            } else {
+                                asyncApi.logMessage(function (error, ...args) {
+                                    if (error) {
+                                        logger.log('An error occurred!', error);
+                                        callback(error, lastState);
+                                    } else {
+                                        callback(null, lastState);
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
             }
-
-            callback(error, lastState);
-        }
-
-        const logMessage = errorOrNext(asyncApi.logMessage, finishAsyncProcess);
-        const alwaysFails = errorOrNext(asyncApi.alwaysFails, logMessage);
-        const doubleNumbers = errorOrNext(asyncApi.doubleNumbers, alwaysFails);
-
-        asyncApi.getNumbers(doubleNumbers);
+        });
     }
-
+    
     return {
         doAsyncStuff: doAsyncStuff
     };
