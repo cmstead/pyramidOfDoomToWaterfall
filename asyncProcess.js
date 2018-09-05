27042bee7f7f70a1a892b33c3283c29fbe58abb5
Chris Stead <cmstead@gmail.com>
2018-09-05 11:44:42 -0700

    Lift log message function

--- asyncProcess.js
+++ asyncProcess.js
@@ -5,26 +5,28 @@ function asyncProcess(asyncApi, logger) {
     function doAsyncStuff(callback) {
         let lastState = null;
 
+        function logMessage(error, ...args) {
+            if (error) {
+                logger.log('An error occurred!', error);
+                callback(error, lastState);
+            } else {
+                asyncApi.logMessage(function (error, ...args) {
+                    if (error) {
+                        logger.log('An error occurred!', error);
+                        callback(error, lastState);
+                    } else {
+                        callback(null, lastState);
+                    }
+                });
+            }
+        }
+
         function alwaysFails(error, ...args) {
             if (error) {
                 logger.log('An error occurred!', error);
                 callback(error, lastState);
             } else {
-                asyncApi.alwaysFails(function (error, ...args) {
-                    if (error) {
-                        logger.log('An error occurred!', error);
-                        callback(error, lastState);
-                    } else {
-                        asyncApi.logMessage(function (error, ...args) {
-                            if (error) {
-                                logger.log('An error occurred!', error);
-                                callback(error, lastState);
-                            } else {
-                                callback(null, lastState);
-                            }
-                        });
-                    }
-                });
+                asyncApi.alwaysFails(logMessage);
             }
         }
 
