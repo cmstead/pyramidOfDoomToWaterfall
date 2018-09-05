9aa2e2942c08c93fadde9f68f8301d92ee1d03c5
Chris Stead <cmstead@gmail.com>
2018-09-05 11:46:42 -0700

    Lift finish async process function

--- asyncProcess.js
+++ asyncProcess.js
@@ -5,19 +5,21 @@ function asyncProcess(asyncApi, logger) {
     function doAsyncStuff(callback) {
         let lastState = null;
 
+        function finishAsyncProcess(error, ...args) {
+            if (error) {
+                logger.log('An error occurred!', error);
+                callback(error, lastState);
+            } else {
+                callback(null, lastState);
+            }
+        }
+
         function logMessage(error, ...args) {
             if (error) {
                 logger.log('An error occurred!', error);
                 callback(error, lastState);
             } else {
-                asyncApi.logMessage(function (error, ...args) {
-                    if (error) {
-                        logger.log('An error occurred!', error);
-                        callback(error, lastState);
-                    } else {
-                        callback(null, lastState);
-                    }
-                });
+                asyncApi.logMessage(finishAsyncProcess);
             }
         }
 
