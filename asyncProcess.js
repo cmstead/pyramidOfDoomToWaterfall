8f468489f41369fb6f64035d933dfd5c0a5efb0d
Chris Stead <cmstead@gmail.com>
2018-09-07 10:27:33 -0700

    Rolled all the way back to the beginning again

--- asyncProcess.js
+++ asyncProcess.js
@@ -3,40 +3,30 @@
 function asyncProcess(asyncApi, logger) {
 
     function doAsyncStuff(callback) {
-        let lastState = null;
-
         asyncApi.getNumbers(function (error, ...getNumbersArgs) {
             if (error) {
                 logger.log('An error occurred!', error);
-                callback(error, lastState);
+                callback(error);
             } else {
-                lastState = getNumbersArgs;
-
                 asyncApi.doubleNumbers.apply(null, getNumbersArgs.concat([
                     function (error, ...doubleNumbersArgs) {
                         if (error) {
                             logger.log('An error occurred!', error);
-                            callback(error, lastState);
+                            callback(error);
                         } else {
-                            lastState = doubleNumbersArgs;
-
                             asyncApi.alwaysFails.apply(null, doubleNumbersArgs.concat([
                                 function (error, ...alwaysFailsArgs) {
                                     if (error) {
                                         logger.log('An error occurred!', error);
-                                        callback(error, lastState);
+                                        callback(error);
                                     } else {
-                                        lastState = alwaysFailsArgs;
-
                                         asyncApi.logMessage.apply(null, alwaysFailsArgs.concat([
                                             function (error, ...logMessageArgs) {
                                                 if (error) {
                                                     logger.log('An error occurred!', error);
-                                                    callback(error, lastState);
+                                                    callback(error);
                                                 } else {
-                                                    lastState = logMessageArgs;
-
-                                                    callback(null, lastState);
+                                                    callback(null, logMessageArgs);
                                                 }
                                             }
                                         ]));
--- tests/asyncProcess.test.js
+++ tests/asyncProcess.test.js
@@ -15,13 +15,22 @@ describe('Async Process: doAsyncStuff', function () {
             log: sinon.spy()
         };
 
+        sinon.spy(asyncApi, 'alwaysFails');
+        sinon.spy(asyncApi, 'logMessage');
+
         doAsyncStuff = asyncProcess(asyncApi, loggerFake).doAsyncStuff;
     });
 
     it('Gets numbers, doubles them and then fails', function (done) {
         doAsyncStuff(function (error, data) {
+            const alwaysFailsArgs = asyncApi.alwaysFails.args[0];
+            const stateBeforeFailure = alwaysFailsArgs.slice(0, alwaysFailsArgs.length - 1);
+
+            assert.equal(JSON.stringify(stateBeforeFailure), '[2,4,6,8,10]');
+
+            assert.equal(asyncApi.logMessage.callCount, 0);
+            
             assert.equal(error.message, 'I always fail! Why did you call me?');
-            assert.equal(JSON.stringify(data), '[2,4,6,8,10]');
 
             assert.equal(loggerFake.log.callCount, 1);
             assert.equal(loggerFake.log.args[0][0], 'An error occurred!');
