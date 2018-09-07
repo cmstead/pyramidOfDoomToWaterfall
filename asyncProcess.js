d27533936a2d21ff1a48d47d4007f2e1bad3e935
Chris Stead <cmstead@gmail.com>
2018-09-07 10:37:54 -0700

    Lifted and named all anonymous functions

--- asyncProcess.js
+++ asyncProcess.js
@@ -3,41 +3,49 @@
 function asyncProcess(asyncApi, logger) {
 
     function doAsyncStuff(callback) {
-        asyncApi.getNumbers(function (error, ...getNumbersArgs) {
+        function finallyAction(error, ...logMessageArgs) {
+            if (error) {
+                logger.log('An error occurred!', error);
+                callback(error);
+            } else {
+                callback(null, logMessageArgs);
+            }
+        }
+
+        function errorOrLogMessage(error, ...alwaysFailsArgs) {
+            if (error) {
+                logger.log('An error occurred!', error);
+                callback(error);
+            } else {
+                asyncApi.logMessage.apply(null, alwaysFailsArgs.concat([
+                    finallyAction
+                ]));
+            }
+        }
+
+        function errorOrAlwaysFails(error, ...doubleNumbersArgs) {
+            if (error) {
+                logger.log('An error occurred!', error);
+                callback(error);
+            } else {
+                asyncApi.alwaysFails.apply(null, doubleNumbersArgs.concat([
+                    errorOrLogMessage
+                ]));
+            }
+        }
+
+        function errorOrDoubleNumbers(error, ...getNumbersArgs) {
             if (error) {
                 logger.log('An error occurred!', error);
                 callback(error);
             } else {
                 asyncApi.doubleNumbers.apply(null, getNumbersArgs.concat([
-                    function (error, ...doubleNumbersArgs) {
-                        if (error) {
-                            logger.log('An error occurred!', error);
-                            callback(error);
-                        } else {
-                            asyncApi.alwaysFails.apply(null, doubleNumbersArgs.concat([
-                                function (error, ...alwaysFailsArgs) {
-                                    if (error) {
-                                        logger.log('An error occurred!', error);
-                                        callback(error);
-                                    } else {
-                                        asyncApi.logMessage.apply(null, alwaysFailsArgs.concat([
-                                            function (error, ...logMessageArgs) {
-                                                if (error) {
-                                                    logger.log('An error occurred!', error);
-                                                    callback(error);
-                                                } else {
-                                                    callback(null, logMessageArgs);
-                                                }
-                                            }
-                                        ]));
-                                    }
-                                }
-                            ]));
-                        }
-                    }
+                    errorOrAlwaysFails
                 ]));
             }
-        });
+        }
+
+        asyncApi.getNumbers(errorOrDoubleNumbers);
     }
 
     return {
